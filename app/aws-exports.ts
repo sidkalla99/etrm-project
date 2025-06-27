1f7d446ee0237537288811c0fd265d2c344bf980
sidkalla99 <siddharth.kalla@zelestra.energy>
2025-06-27 08:41:51 +0530

    Create aws-exports.ts
    added aws cognito configs

---
+++ app/aws-exports.ts
@@ -0,0 +1,11 @@
+// src/aws-exports.ts
+const awsconfig = {
+  Auth: {
+    region: 'eu-central-1', // e.g. 'us-east-1'
+    userPoolId: 'eu-central-1_aMNw7M2Dy',
+    userPoolWebClientId: '4dt7mhhoi7s11aabf494rloroh',
+    mandatorySignIn: false,
+  }
+};
+
+export default awsconfig;
